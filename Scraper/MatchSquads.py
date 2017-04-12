9690b3354f90ff6ed0e4c87a5e93396af79deeb0
shriya17 <shriya.mishra1704@gmail.com>
2017-04-12 19:44:55 +0530

    Updated MatchSquad.py

--- Scraper/MatchSquads.py
+++ Scraper/MatchSquads.py
@@ -9,38 +9,50 @@ from selenium.webdriver.common.keys import Keys
 
 """
 
-def fetchSquadList(col_name):
+def fetchSquadList(col_name,text_file):
+    time.sleep(0.3)
     column = browser.find_element_by_css_selector(col_name)
+    col_header = column.find_element_by_class_name("position")
+    text_file.write("*************** " + col_header.text + "*****************\n")
     col_list = column.find_elements_by_class_name("info")
     cnt = 0
     for line in col_list:
         if cnt < 12:
-            print line.find_element_by_class_name("name").text + "  -  " + line.find_element_by_class_name("position").text
-            cnt = cnt+1
+            try:
+                sub_out = line.find_element_by_css_selector(".icn.sub-off")
+                text_file.write(line.find_element_by_class_name("name").text + "  -  " + line.find_element_by_class_name(
+                        "position").text + " so" + '\n')
+
+                cnt += 1
+            except:
+                text_file.write(line.find_element_by_class_name("name").text + "  -  " + line.find_element_by_class_name(
+                    "position").text + '\n')
+                cnt += 1
+
         else:
             try:
                 sub_in = line.find_element_by_css_selector(".icn.sub-on")
                 if sub_in is not None:
-                    print line.find_element_by_class_name("name").text + "  -  " + line.find_element_by_class_name(
-                        "position").text
+                    text_file.write(line.find_element_by_class_name("name").text + "  -  " + line.find_element_by_class_name(
+                        "position").text+" si " + '\n')
             except:
-                continue
+                print "Fetch failed Sub-on"
 
 
 
 
 
 def getSquadUrl(fileName):
-    text_write = open("MatchSquad-2013.txt",'a')
+    text_file = open("MatchSquad-2013.txt",'w')
     with open(fileName,'r') as list:
         for line in list:
             try:
                 browser.get("https://www.premierleague.com/match/" + line)
                 tab = browser.find_element_by_class_name("matchCentreSquadLabelContainer")
                 tab.click()
-                time.sleep(0.1)
-                fetchSquadList(".col-4-m ")
-                fetchSquadList(".col-4-m.right")
+                time.sleep(0.3)
+                fetchSquadList(".col-4-m ",text_file)
+                fetchSquadList(".col-4-m.right",text_file)
             except:
                 continue
 
