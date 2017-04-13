c942270f1580b75f2c80e088ce9bb6293e68893d
Sourabh Swain <sourabh.swain@sap.com>
2017-04-13 15:56:40 +0530

    ScoreBoard_Detailed_2013.csv

--- Scraper/Scoreboard_Detailed.py
+++ Scraper/Scoreboard_Detailed.py
@@ -9,6 +9,51 @@ Script Description:
 
 """
 
+def transferToCsv(data,fileName):
+
+    """
+    :param data:string
+    :param fileName:string
+    :param season:string
+    :return:none
+
+    This function takes the listoflists (data) and adds the corresponding matches for a particular seasons
+    into the variable data. After that, the data variable is written line by line into the Scoreboard_all.csv file
+    """
+
+    with open(fileName, "r") as ins:
+        for line in ins:
+            val = str(line).rstrip('\n')
+            #print val
+            x = val.split(",")
+            #print x
+            data.append(x)
+
+    #print data
+
+def convertToCSV(fileName):
+    data = []
+
+    # Initializing a blank .csv file
+    with open("ScoreBoard_Detailed_2013.csv", "wb") as csv_file:
+        writer = csv.writer(csv_file, delimiter=',')
+        for line in data:
+            writer.writerow(line)
+
+    csv_file.close()
+
+    # Appending the Column data for the .csv file
+    data.append("MId,Home Team,Home Team Goals,Away Team Goals,Away Team,Home Poss,Away Poss,Home ShotsT,Away ShotsT,Home Shots,Away Shots,Home Touches,Away Touches,Home Passes,Away Passes,Home Tackles,Away Tackles,Home Clearances,Away Clearances,Home Corners,Away Corners,Home Offsides,Away Offsides".split(","))
+    transferToCsv(data,fileName)
+    with open("ScoreBoard_Detailed_2013.csv", "ab") as csv_file:  # Opens the .csv file in append binary mode.
+        writer = csv.writer(csv_file, delimiter=',')
+        for line in data:
+            #print line
+            writer.writerow(line)
+
+    csv_file.close()
+
+
 def addStats(val,matchId):
     myList = [item for item in val.split('\n')]
     newString = ''.join(myList)
@@ -55,12 +100,12 @@ with open("ScoreBoard_2013.txt","r") as ins:
         key_at_index = statsList.keys()[idx]
         val = str(line).rstrip('\n')
         val = key_at_index + "," + val
-        val = val + "," + value_at_index
+        val = val + value_at_index
         val = val.rstrip(',')
-        #print val
         text_file.write(val + '\n')
         idx += 1
 
 text_file.close()
+convertToCSV("ScoreBoard_Detailed_2013.txt")
 
 
