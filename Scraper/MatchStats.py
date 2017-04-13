d296e1e271c6cfe8e3187aa466f0a9428c87ad05
Sourabh Swain <sourabh.swain@sap.com>
2017-04-13 17:10:40 +0530

    Update

--- Scraper/MatchStats.py
+++ Scraper/MatchStats.py
@@ -1,6 +1,7 @@
 import time
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import StaleElementReferenceException
 
 
 """
@@ -17,15 +18,15 @@ from selenium.webdriver.common.keys import Keys
 """
 
 browser = webdriver.Chrome()
-text_file = open("Stats-2013.txt","w")
+text_file = open("Stats-2014.txt","w")
 
-with open("Matches-2013.txt", "r") as ins:
+with open("Matches-2014.txt", "r") as ins:
     for line in ins:
-        browser.get("https://www.premierleague.com/match/" + line)
-        elem = browser.find_element_by_tag_name("body")
-        elem.send_keys(Keys.PAGE_DOWN)
-        time.sleep(1)
         try:
+            browser.get("https://www.premierleague.com/match/" + line)
+            elem = browser.find_element_by_tag_name("body")
+            elem.send_keys(Keys.PAGE_DOWN)
+            time.sleep(1)
             tab = browser.find_element_by_class_name("tablist")
             stats = tab.find_element_by_xpath(".//li[not(@class)]")
             stats.click()
@@ -38,12 +39,16 @@ with open("Matches-2013.txt", "r") as ins:
 
             time.sleep(1)
             stats = browser.find_element_by_class_name("matchCentreStatsContainer")
-        except:
+            text_file.write("Match: " + line)
+            text_file.write(stats.text + "\n\n")
+        except StaleElementReferenceException:
             text_file.write("Match: " + line)
             for x in range(0, 10):
-                text_file.write("XX-XX-XX-XX-XX-XX\n")
+                text_file.write("XX-XX-,XX-XX-,XX-XX\n")
             text_file.write("Match: " + line)
             text_file.write(stats.text + "\n\n")
 
             text_file.write("\n\n")
+        except:
+            print line
 
