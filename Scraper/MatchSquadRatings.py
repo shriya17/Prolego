01217780a049ca3ca13a355393e3a5d2f74c83f5
Sourabh Swain <sourabh.swain@sap.com>
2017-05-26 18:18:47 +0530

    Cleaned Match Squads text file

--- Scraper/MatchSquadRatings.py
+++ Scraper/MatchSquadRatings.py
@@ -1,8 +1,42 @@
+import string
+
 """
 
 This
 
 """
 
-squad_file = open("MatchSquads-2013.txt","r")
-ratings_file = open("PlayerRatings-2013-Final.txt","r")
+def cleanFile(text_file):
+    with open(text_file,"r") as fin, open("MatchSquads-2015_final.txt","w") as fout:
+        flag = 0
+        temp = ""
+        for line in fin:
+            var = str(line)
+            var = var.rstrip('\n')
+            if "Positions" not in var and "END MATCH" not in var and "NEXT TEAM" not in var and "..." not in var and "Substitutes" not in var:
+                if flag == 0:
+                    temp = ""
+                    temp += var
+                    flag = 1
+                elif flag == 1:
+                    if "Goalkeeper" in var or "Defender" in var or "Midfielder" in var or "Forward" in var:
+                        temp = temp + " " + var
+                        flag = 0
+                        fout.write(temp + '\n')
+                    else:
+                        temp = temp + " " + var
+            else:
+                fout.write(var + '\n')
+
+    fin.close()
+    fout.close()
+
+
+#Text file open
+#ratings_file = open("PlayerRatings-2013-Final.txt","r")
+cleanFile("MatchSquads-2015.txt")
+
+#Removing squad numbers
+with open("MatchSquads-2013_final.txt","r") as fin:
+    for line in fin:
+        var = str(line)
